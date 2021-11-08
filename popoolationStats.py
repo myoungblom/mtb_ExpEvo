292c8cc4f695cec289cd67effba68a45557dfddf
Madison <youngblom@wisc.edu>
2021-11-08 08:41:06 -0600

    minor changes

--- popoolationStats.py
+++ popoolationStats.py
@@ -24,12 +24,11 @@ outfile = sys.argv[1]
 def popStats(bam,resultDict):
     # make pileup file using Samtools
     sample = bam.split(".")[0]
-    #pilefile = sample+".pileup"
-    pilefile = "31-00.pileup"
-    #with open(pilefile,"w") as out:
-        #print("Converting bam to pileup ...")
-        #subprocess.call(["/opt/PepPrograms/samtools-1.11/samtools","mpileup","-B","-f",\
-            #"/opt/data/mtuberculosis/MtbNCBIH37Rv.fa",bam],stdout=out)
+    pilefile = sample+".pileup"
+    with open(pilefile,"w") as out:
+        print("Converting bam to pileup ...")
+        subprocess.call(["/opt/PepPrograms/samtools-1.11/samtools","mpileup","-B","-f",\
+            "/opt/data/mtuberculosis/MtbNCBIH37Rv.fa",bam],stdout=out)
     
     # subsample pileup file 10X
     pi = []
@@ -37,25 +36,24 @@ def popStats(bam,resultDict):
     tajimasd = []
     for x in range(1,2):
         print("subsample #"+str(x)+" of "+pilefile+" ...")
-        #subfile = bam.split(".")[0]+"_subsampled_"+str(x)+".pileup"
-        subfile = "31-00_subsampled_1.pileup"
-        #subprocess.call(["perl","/opt/PepPrograms/popoolation_1.2.2/basic-pipeline/subsample-pileup.pl",\
-            #"--input",pilefile,"--output",subfile,"--target-coverage","50","--max-coverage","1000",\
-                #"--min-qual","20","--fastq-type","sanger","--method","withoutreplace"])
+        subfile = bam.split(".")[0]+"_subsampled_"+str(x)+".pileup"
+        subprocess.call(["perl","/opt/PepPrograms/popoolation_1.2.2/basic-pipeline/subsample-pileup.pl",\
+            "--input",pilefile,"--output",subfile,"--target-coverage","50","--max-coverage","1000",\
+                "--min-qual","20","--fastq-type","sanger","--method","withoutreplace"])
         # calculate pi and Tajima's D for each subsampled pileup file
         pifile = subfile.split(".")[0]+".pi"
         dfile = subfile.split(".")[0]+".d"
         tfile = subfile.split(".")[0]+".theta"
         print("Calculating stats ...")
-        #subprocess.call(["perl","/opt/PepPrograms/popoolation_1.2.2/Variance-sliding.pl","--measure","pi","--pool-size",\
-            #"10000","--fastq-type","sanger","--min-count","2","--window-size","100000","--step-size",\
-                #"10000","--input",subfile,"--output",pifile])
-        #subprocess.call(["perl","/opt/PepPrograms/popoolation_1.2.2/Variance-sliding.pl","--measure","D","--pool-size",\
-            #"10000","--fastq-type","sanger","--min-count","2","--window-size","100000","--step-size",\
-                #"10000","--input",subfile,"--output",dfile])
-        #subprocess.call(["perl","/opt/PepPrograms/popoolation_1.2.2/Variance-sliding.pl","--measure","theta","--pool-size",\
-            #"10000","--fastq-type","sanger","--min-count","2","--window-size","100000","--step-size",\
-                #"10000","--input",subfile,"--output",tfile])
+        subprocess.call(["perl","/opt/PepPrograms/popoolation_1.2.2/Variance-sliding.pl","--measure","pi","--pool-size",\
+            "10000","--fastq-type","sanger","--min-count","2","--window-size","100000","--step-size",\
+                "10000","--input",subfile,"--output",pifile])
+        subprocess.call(["perl","/opt/PepPrograms/popoolation_1.2.2/Variance-sliding.pl","--measure","D","--pool-size",\
+            "10000","--fastq-type","sanger","--min-count","2","--window-size","100000","--step-size",\
+                "10000","--input",subfile,"--output",dfile])
+        subprocess.call(["perl","/opt/PepPrograms/popoolation_1.2.2/Variance-sliding.pl","--measure","theta","--pool-size",\
+            "10000","--fastq-type","sanger","--min-count","2","--window-size","100000","--step-size",\
+                "10000","--input",subfile,"--output",tfile])
         # average stats across windows and replicates
         pi.append(list(pd.read_csv(pifile,sep="\t").iloc[:,4]))
         theta.append(list(pd.read_csv(tfile,sep="\t").iloc[:,4]))
